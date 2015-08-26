a88127970a3132bde38da2abc09ac638f1eca472
loitv <loitran1763@gmail.com>
2015-08-27 06:07:57 +0700

    Day 3 - Basic OOP

---
+++ src/Day3/MainProcess.java
@@ -0,0 +1,49 @@
+package Day3;
+
+import java.util.Scanner;
+
+public class MainProcess {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Subject sbj = new Subject();
+		String yn = "Y";
+		boolean quit = false;
+
+		sbj.setSubjectID("IT3650");
+		sbj.setSubjectName("Lap trinh Java");
+		sbj.setQuota(5);
+		sbj.setCurrentEnrolment(0);
+
+		while (!quit) {
+			System.out.println("Dang ki mon hoc Lap trinh Java");
+			System.out.println("1. Dang ki");
+			System.out.println("2. Huy dang ki");
+			System.out.println("3. Hien thi thong tin dang ki");
+			System.out.println("4. Thoat");
+			String str = sc.nextLine();
+			int choose = Integer.parseInt(str);
+
+			if (choose == 1) {
+				do {
+					sbj.enrolStudent();
+					System.out.println("So sinh vien dang ki hien tai: " + sbj.getCurrentEnrolment());
+					System.out.println("Do you want to continues (Y/N)?");
+					yn = sc.nextLine();
+				} while (yn.equalsIgnoreCase("Y"));
+			} else if (choose == 2) {
+				do {
+					sbj.unEnrolStudent();
+					System.out.println("So sinh vien da dang ki: " + sbj.getCurrentEnrolment());
+					System.out.println("Do you want to continues (Y/N)?");
+					yn = sc.nextLine();
+				} while (yn.equalsIgnoreCase("Y"));
+			} else if (choose == 3) {
+				sbj.displaySubjectInfo();
+				System.out.println();
+			} else {
+				quit = true;
+			}
+		}
+	}
+}
---
+++ src/Day3/Subject.java
@@ -0,0 +1,79 @@
+package Day3;
+
+public class Subject {
+
+	private String subjectID;
+	private String subjectName;
+	private int quota;
+	private int currentEnrolment;
+	
+	public Subject() {
+		this.quota = 80;
+	}
+	public Subject(String initID, String initName, int initQuota, int initCurrentEnrolment) {
+		this.subjectID = new String(initID);
+		this.subjectName = new String(initName);
+		this.quota = initQuota;
+		this.currentEnrolment = initCurrentEnrolment;
+	}
+	//Generate getters and setters
+	public String getSubjectID() {
+		return subjectID;
+	}
+	public void setSubjectID(String subjectID) {
+		this.subjectID = subjectID;
+	}
+	public String getSubjectName() {
+		return subjectName;
+	}
+	public void setSubjectName(String subjectName) {
+		this.subjectName = subjectName;
+	}
+	public int getQuota() {
+		return quota;
+	}
+	public void setQuota(int quota) {
+		this.quota = quota;
+	}
+	public int getCurrentEnrolment() {
+		return currentEnrolment;
+	}
+	public void setCurrentEnrolment(int currentEnrolment) {
+		this.currentEnrolment = currentEnrolment;
+	}
+	
+	//Enrols student into course
+	public void enrolStudent() {
+		System.out.println("Enrol Student.");
+		if (currentEnrolment < quota) {
+			++currentEnrolment;
+			System.out.println("Student enrolled in: " + subjectName);
+		}
+		else {
+			System.out.println("Quota reached! Enrolment failed!");
+		}
+	}
+	
+	//Unenrols Student
+	public void unEnrolStudent() {
+		System.out.println("Un-Enrolling Student.");
+		if (currentEnrolment <= 0) {
+			System.out.println("No Student to Un-enrol");
+		}
+		else {
+			--currentEnrolment;
+			System.out.println("Student un-enrol from " + subjectName);
+		}
+	}
+	
+	//Display subject info
+	public void displaySubjectInfo() {
+		System.out.println("Subject ID " + subjectID);
+		System.out.println("Subject Name " + subjectName);
+		System.out.println("Quota " + quota);
+		System.out.println("Current Enrolment " + currentEnrolment);
+		int availablePlaces = quota - currentEnrolment;
+		System.out.println("Can appcept " + availablePlaces + " more students");
+	}
+}
+
